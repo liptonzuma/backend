84d2706122dae69a24f7ec6485887034600ab427
liptonzuma <solomonboateng231@gmail.com>
2021-10-19 11:28:33 +0000

    post method

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser') 
 require('dotenv').config()
 
+app.use(bodyParser());
+app.use(express.json())
 const port = process.env.PORT || 5000
+
+const users =  []
+
+app.post('/',(req,res)=>{
+
+   const username = req.body.username;
+   const age = req.body.age;
+   const email = req.body.email;
+
+   const user = {
+       username,
+       age,
+       email,
+   }
+
+   users.push(user);
+   console.log(users);
+})
 
 app.listen(port,()=>{
     console.log(`Server connected successfully on port ${port}`)
