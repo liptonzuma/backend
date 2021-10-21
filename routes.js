8654793e3ced449a7e2207fd6709555ab3177f9b
liptonzuma <solomonboateng231@gmail.com>
2021-10-21 01:40:58 +0000

    put request

--- routes.js
+++ routes.js
@@ -7,14 +7,21 @@ const user  = require('./schema');
 router.get('/',(req,res)=>{
     res.send('Welcome')
 })
-
+// deleting a particular data
 router.delete('/:id',(req,res)=>{
 
     user.findByIdAndRemove({_id:req.params.id})
-    
+
     .then((data)=>{
         res.send(data)
         console.log(data)
+    })
+})
+// updating a current user's data
+router.put('/:id',(req,res)=>{
+    user.findByIdAndUpdate({_id:req.params.id},req.body)
+    .then(()=>{
+        user.findOne({_id:req.params.id}).then(data=>res.send(data))
     })
 })
 
